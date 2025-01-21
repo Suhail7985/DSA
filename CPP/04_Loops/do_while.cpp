bc3ff7788cce059e1ac23a2c70d96b8f7d71ef38
Shubh07-Cs <srai21029@gmail.com>
2025-01-21 21:48:56 +0530

    update

---
+++ CPP/04_Loops/do_while.cpp
@@ -0,0 +1,34 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+//     // This is an infinite do while loop as the condition
+//     // expression is always true
+//     do {
+//         cout << "This loop will run forever.\n";
+//     }while(1);
+//     return 0;
+// }
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+      // Initialization
+      int i = 1;
+  
+      // while loop that starts with i = 1 and ends
+      // when i is greater than 5.
+     do {
+        cout << i << " ";
+      
+          // Updation
+          i++;
+    }while (i <= 5);
+
+    return 0;
+}
