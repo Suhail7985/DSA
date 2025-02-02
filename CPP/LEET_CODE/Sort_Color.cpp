47ef0723b518997a571b6c6e682fa48f4c8c875f
Suhail7985 <suhailmansoori7985@gmail.com>
2025-02-02 17:48:54 +0530

    2febuar

--- CPP/LEET_CODE/Sort_Color.cpp
+++ CPP/LEET_CODE/Sort_Color.cpp
@@ -3,19 +3,25 @@
 #include<vector>
 using namespace std;
 void sortColors(vector<int>& nums){
-    int n=nums.size();
-    int j=1;
-    for(int i=0;i<n;i++){
-        if(nums[i]>=nums[j]){
-            swap(nums[i],nums[j]);
-
-            j++;
-
+    int low=0;
+    int mid=0;
+    int high=nums.size()-1;
+    while(mid<=high){
+        if(nums[mid]==0){
+            swap(nums[low],nums[mid]);
+            low++;
+            mid++;
         }
-
+        else if(nums[mid]==1){
+            mid++;
+        }
+        else{
+            swap(nums[mid],nums[high]);
+            high--;
+        }
     }
-
 }
+
 int main(){
     vector<int> nums={2,0,2,1,1,0};
     sortColors(nums);
