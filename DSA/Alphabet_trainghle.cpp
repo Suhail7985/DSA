a230d585846fdddcd8dccc4d5e621f2e1a81490b
Suhail7985 <suhailmansoori7985@gmail.com>
2025-12-24 15:11:33 +0530

    first commit

---
+++ DSA/Alphabet_trainghle.cpp
@@ -0,0 +1,16 @@
+#include<iostram>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        char ch = 'A';
+        for(int j=0;j<=i;j++){
+            cout<<ch<<" ";
+            ch++;
+
+        }
+        cour<<endl;
+    }
+    return 0;
+}
