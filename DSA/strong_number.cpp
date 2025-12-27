651926d37d351d4537fa4b908a6b6b4816dde93d
Suhail7985 <suhailmansoori7985@gmail.com>
2025-12-27 15:22:14 +0530

    first commit

--- DSA/strong_number.cpp
+++ DSA/strong_number.cpp
@@ -1,28 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int factorial(int n){
-    if (n==1){
-         return 1;
-    }
-    return n*factorial(n-1);
-   
+
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    return n * factorial(n - 1);
 }
-int main(){
-    int n, temp, sum=0, d;
-    cout<<"Enter number: ";
-    cin>>n;
+
+int main() {
+    int n, temp, sum = 0, d;
+
+    cout << "Enter number: ";
+    cin >> n;
+
     temp = n;
-    while(temp){
-        d= temp % 10;
+    while (temp > 0) {
+        d = temp % 10;
         sum += factorial(d);
-        temp/=10;
+        temp /= 10;
     }
-    if(sum==n){
-        cout<<"Number is strong";
 
-    } 
-    else{
-        cout<<"number is not strong";
-    }
+    if (sum == n)
+        cout << "Number is strong";
+    else
+        cout << "Number is not strong";
+
+    return 0;
 }
-
